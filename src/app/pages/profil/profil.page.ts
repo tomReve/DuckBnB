8ca43af5769ef819d9d9060b79f043cc209c6b09
Tom Reversez <tom.reversez@gmail.com>
2020-02-13 16:19:11 +0100

    Logout

--- src/app/pages/profil/profil.page.ts
+++ src/app/pages/profil/profil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
+  }
+  disconnect() {
+    this.storage.set('userAuthenticated', false);
+    this.router.navigate(['/login']);
   }
 
 }
