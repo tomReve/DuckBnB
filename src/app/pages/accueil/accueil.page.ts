da9ae63a7a68352882da67c36207966338a64b41
Tom Reversez <tom.reversez@gmail.com>
2020-02-13 10:26:46 +0100

    Fix img

--- src/app/pages/accueil/accueil.page.ts
+++ src/app/pages/accueil/accueil.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {LogementService} from '../../services/logement.service';
-import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-accueil',
@@ -10,7 +9,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 export class AccueilPage implements OnInit {
 
-  constructor(private router: Router, public logementService: LogementService, public domSanitizer: DomSanitizer) { }
+  constructor(private router: Router, public logementService: LogementService) { }
 
   ngOnInit() {
     this.logementService.load();
